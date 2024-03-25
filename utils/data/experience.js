9cd57df10662d474665a5f8cf727bf2c84ea824f
Yiyuan Low <yiyuanlow@gmail.com>
2024-03-25 10:11:23 +0800

    update experience

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -10,5 +10,17 @@ export const experiences = [
     title: "IT Provisioning Officer",
     company: "North Metropolitan TAFE",
     duration: "(March 2023 - Feb 2024)"
+  },
+  {
+    id: 3,
+    title: "Intern Web Developer",
+    company: "Wanglong Technologies Sdn. Bhd.",
+    duration: "(November 2021 - Feb 2022)"
+  },
+  {
+    id: 4,
+    title: "Intern Software Engineer",
+    company: "Software International (C) Malaysia Sdn. Bhd.",
+    duration: "(September 2019 - November 2019)"
   }
 ]
