5c55353f9bd56f497bcc122df1da8dfb74f12331
Yiyuan Low <yiyuanlow@gmail.com>
2024-03-26 18:39:13 +0800

    update hero

--- app/components/homepage/hero-section/index.jsx
+++ app/components/homepage/hero-section/index.jsx
@@ -26,7 +26,7 @@ function HeroSection() {
             Hello, <br />
             I am {' '}
             <span className=" text-pink-500">{personalData.name}</span>
-            {` ,  `}
+            {` , a `}
             <span className=" text-[#16f2b3]">{personalData.designation}</span>
             .
           </h1>
